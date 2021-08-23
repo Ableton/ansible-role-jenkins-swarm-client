f4089fb2dc90d15a685dce0750fe30d2e525f1aa
nre <nre@ableton.com>
2021-08-23 13:24:20 +0200

    Reformat Python code with black

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -3,47 +3,50 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
 def test_java_installed(host):
-    assert host.file('/usr/bin/java').exists
+    assert host.file("/usr/bin/java").exists
 
 
 def test_jenkins_user(host):
-    user = host.user('juser')
+    user = host.user("juser")
 
-    assert user.group == 'jgroup'
-    assert user.home == '/jenkins'
-    assert user.shell == '/bin/bash'
+    assert user.group == "jgroup"
+    assert user.home == "/jenkins"
+    assert user.shell == "/bin/bash"
 
 
 def test_jenkins_dir(host):
-    jenkins_dir = host.file('/jenkins')
+    jenkins_dir = host.file("/jenkins")
 
     assert jenkins_dir.is_directory
     assert jenkins_dir.mode == 0o0755
-    assert jenkins_dir.user == 'juser'
-    assert jenkins_dir.group == 'jgroup'
+    assert jenkins_dir.user == "juser"
+    assert jenkins_dir.group == "jgroup"
 
 
 def test_jenkins_swarm_client_config_files(host):
-    swarm_client_config_file = host.file('/jenkins/swarm-client-config.yml')
-    logging_config_file = host.file('/jenkins/logging.properties')
+    swarm_client_config_file = host.file("/jenkins/swarm-client-config.yml")
+    logging_config_file = host.file("/jenkins/logging.properties")
 
     assert swarm_client_config_file.is_file
     assert logging_config_file.is_file
 
 
 def test_swarm_client_running(host):
-    process = host.process.get(comm='java')
+    process = host.process.get(comm="java")
 
     assert process is not None
-    assert process.args == ' '.join([
-        '/usr/bin/java',
-        '-Djava.util.logging.config.file=/jenkins/logging.properties',
-        '-jar',
-        '/jenkins/swarm-client-3.27.jar',
-        '-config',
-        '/jenkins/swarm-client-config.yml'
-    ])
+    assert process.args == " ".join(
+        [
+            "/usr/bin/java",
+            "-Djava.util.logging.config.file=/jenkins/logging.properties",
+            "-jar",
+            "/jenkins/swarm-client-3.27.jar",
+            "-config",
+            "/jenkins/swarm-client-config.yml",
+        ]
+    )
